1c39127a70c6357e099d0c97fa07b2adab10d824
arbytez <55246742+arbytez@users.noreply.github.com>
2020-10-14 20:23:46 +0200

    fix bad async code on uncaughtException/unhandledRejection

--- src/helpers/errorsFallback.ts
+++ src/helpers/errorsFallback.ts
@@ -1,24 +1,12 @@
 import signale from '../logger';
 
-import prismaClient from '../server/prismaClient';
-
 // catch all the unmanaged errors and stop script
 process.on('uncaughtException', async (err) => {
   signale.fatal(err);
-  try {
-    await prismaClient.disconnect();
-  } catch (error) {
-    signale.error(error);
-  }
   process.exit(1);
 });
 
 process.on('unhandledRejection', async (err) => {
   signale.fatal(err);
-  try {
-    await prismaClient.disconnect();
-  } catch (error) {
-    signale.error(error);
-  }
   process.exit(1);
 });
